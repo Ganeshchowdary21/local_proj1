ffc95a8b297eef34c52227a16582842388f42739
Ganeshchowdary21 <77258973+Ganeshchowdary21@users.noreply.github.com>
2021-01-11 12:27:57 +0530

    added subject for server interface

--- cbvproject2/testapp/admin.py
+++ cbvproject2/testapp/admin.py
@@ -3,6 +3,6 @@ from testapp.models import *
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_dispaly=['title','author','pages','price']
+    list_dispaly=['title','author','pages','price','subject']
 
 admin.site.register(Book,BookAdmin)
